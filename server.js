d676231f2c1e67d6bbd207bfb2d20f54c0c74957
mao <maoyuan@gmail.com>
2020-05-11 22:45:12 -0400

    connected to mongo atlas cloud database

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({path: './config.env'});
 const {app} = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASS);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con=>{
+    console.log('db connected')
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`app running on : http://localhost:${port}`)
 });
+
