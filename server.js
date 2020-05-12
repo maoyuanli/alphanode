a2f976c581e4359a0547ac1dce3a613f877415df
mao <maoyuan@gmail.com>
2020-05-12 17:07:09 -0400

    add item to mongodb

--- server.js
+++ server.js
@@ -9,10 +9,37 @@ mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-}).then(con=>{
+}).then(con => {
     console.log('db connected')
 });
 
+const packageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rating: Number,
+    price: {
+        type: Number,
+        required: true
+    },
+    summary: String
+});
+
+const Package = mongoose.model('Package', packageSchema);
+
+const pkg = new Package({
+    name: 'Basic',
+    rating: 4.7,
+    price: 980,
+    summary: 'a basic package'
+});
+
+pkg.save().then((doc) => {
+    console.log(doc)
+}).catch((err) => {
+    console.log(err)
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
