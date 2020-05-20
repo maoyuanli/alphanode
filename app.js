6a5fb47525ff92043e48c096250ec968ec97a73c
mao <maoyuan@gmail.com>
2020-05-20 15:13:40 -0400

    prepares heroku deployment

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const {membershipRouter} = require('./routers/membership-routers');
 const {programRouter} = require('./routers/program-routers');
-
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-
+app.options('*', cors());
 app.use('/api/membership', membershipRouter);
 app.use('/api/program', programRouter);
 
