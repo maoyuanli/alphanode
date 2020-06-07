92e7805c5939869408b8863aa44d77dff80ebfa8
mao <maoyuan@gmail.com>
2020-06-06 22:47:39 -0400

    cannot import dev if it's not even git pushed. uses workaround ( require in if statement )

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,10 +1,10 @@
-import {devConfig} from "./dev";
 import {prodConfig} from "./prod";
 
 export const provideConfig = (): Config => {
     if (typeof process.env.NODE_ENV !== 'undefined' && process.env.NODE_ENV === 'production') {
         return prodConfig;
     } else {
+        const {devConfig} = require('./dev');
         return devConfig;
     }
 };
