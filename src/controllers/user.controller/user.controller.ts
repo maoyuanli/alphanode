3e6771d38c2045332a4a4d9a7c7285c28660bb62
mao <maoyuan@gmail.com>
2020-06-09 16:06:07 -0400

    introduces bcrypt and hashed password for user

--- src/controllers/user.controller/user.controller.ts
+++ src/controllers/user.controller/user.controller.ts
@@ -1,16 +1,22 @@
 import {User} from "../../models/user.model";
 import {Request, Response} from "express";
+import bcrypt from "bcrypt";
 
-export const userRegister = async (req:Request, res:Response) =>{
+export const userRegister = async (req: Request, res: Response) => {
     try {
-        const userRegistered = await User.create(req.body);
+        const username: string = req.body.username;
+        const salt = await bcrypt.genSalt(10);
+        const password = await bcrypt.hash(req.body.password, salt);
+        const userRegistered = await User.create({
+            username, password
+        });
         res.status(200).json({
             status: 'user added',
-            data:{
+            data: {
                 user: userRegistered.get("username")
             }
         })
-    }catch (err) {
+    } catch (err) {
         res.status(400).json({
             status: 'operation failed',
             message: err
